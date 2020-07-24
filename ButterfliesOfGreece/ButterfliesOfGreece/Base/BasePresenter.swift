1715965216e9e0454020fef72740823e11a1f436
Ioannis Kapsouras <jkapsouras@san-soft.com>
2020-07-24 15:56:22 +0300

    Add schedulers

--- ButterfliesOfGreece/ButterfliesOfGreece/Base/BasePresenter.swift
+++ ButterfliesOfGreece/ButterfliesOfGreece/Base/BasePresenter.swift
@@ -5,5 +5,33 @@
 //  Created by Ioannis Kapsouras on 5/7/20.
 //  Copyright © 2020 Ioannis Kapsouras. All rights reserved.
 //
-
+import RxSwift
 import Foundation
+
+class BasePresenter{
+    
+	   var disposeBag:DisposeBag=DisposeBag()
+		var backgroundThreadScheduler:BackgroundThreadProtocol
+		var mainThreadScheduler:MainThreadProtocol
+//		var  state:PublishSubject<ViewState> = PublishSubject()
+//
+		init(backScheduler:BackgroundThreadProtocol,mainScheduler:MainThreadProtocol)
+		{
+			backgroundThreadScheduler=backScheduler
+			mainThreadScheduler=mainScheduler
+		}
+//
+//		func HandleEvent(uiEvent:UiEvent)
+//		{
+//
+//		}
+//
+//		public func Subscribe(events:Observable<UiEvent>)->Observable<ViewState>
+//		{
+//			events
+//				.subscribe(onNext: { event in self.HandleEvent(uiEvent: event)})
+//				.disposed(by: disposeBag)
+//
+//			return state.asObservable().observeOn(mainThreadScheduler.scheduler)
+//		}
+}
