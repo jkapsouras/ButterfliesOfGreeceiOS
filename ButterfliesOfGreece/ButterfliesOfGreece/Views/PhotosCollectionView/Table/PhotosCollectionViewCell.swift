7c2b4ee6a48379bdf0e435d71a437768b25a317e
Ioannis Kapsouras <jkapsouras@san-soft.com>
2020-08-13 16:14:07 +0300

    test lfs

--- ButterfliesOfGreece/ButterfliesOfGreece/Views/PhotosCollectionView/Table/PhotosCollectionViewCell.swift
+++ ButterfliesOfGreece/ButterfliesOfGreece/Views/PhotosCollectionView/Table/PhotosCollectionViewCell.swift
@@ -2,12 +2,17 @@
 //  PhotosCollectionViewCell.swift
 //  ButterfliesOfGreece
 //
-//  Created by Ioannis Kapsouras on 11/8/20.
+//  Created by Ioannis Kapsouras on 13/8/20.
 //  Copyright © 2020 Ioannis Kapsouras. All rights reserved.
 //
 
 import UIKit
 
 class PhotosCollectionViewCell: UICollectionViewCell {
-    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
 }
