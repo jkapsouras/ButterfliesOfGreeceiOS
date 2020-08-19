9a315535688b99762feffc2e28834b2775c00dcb
Yannis Kapsouras <jkapsouras@gmail.com>
2020-08-19 23:03:29 +0300

    init species feature

---
+++ ButterfliesOfGreece/ButterfliesOfGreece/ViewControllers/SpeciesViewController/SpeciesPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  SpeciesPresenter.swift
+//  ButterfliesOfGreece
+//
+//  Created by Apprecot on 19/8/20.
+//  Copyright © 2020 Ioannis Kapsouras. All rights reserved.
+//
+
+import Foundation
---
+++ ButterfliesOfGreece/ButterfliesOfGreece/ViewControllers/SpeciesViewController/UiEvents/SpeciesEvents.swift
@@ -0,0 +1,15 @@
+//
+//  SpeciesEvents.swift
+//  ButterfliesOfGreece
+//
+//  Created by Apprecot on 19/8/20.
+//  Copyright © 2020 Ioannis Kapsouras. All rights reserved.
+//
+
+import Foundation
+
+enum SpeciesEvents:UiEvent{
+	case loadFamilies
+	case specieClicked(id:Int)
+	case addPhotosForPrintClicked(specieId:Int)
+}
