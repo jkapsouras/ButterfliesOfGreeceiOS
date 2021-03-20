ba32d370f2ea2cad7c2dd12a2600eb64bcbf750d
Yannis Kapsouras <jkapsouras@gmail.com>
2021-03-20 13:53:17 +0200

    fix prebuild script

--- ButterfliesOfGreece/appcenter-pre-build.sh
+++ ButterfliesOfGreece/appcenter-pre-build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-CUR_COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ios/Podfile.lock`
-ENV_COCOAPODS_VER=`pod --version`
 
-# check if not the same version, reinstall cocoapods version to current project's
-if [ $CUR_COCOAPODS_VER != $ENV_COCOAPODS_VER ];
-then
-    echo "Uninstalling all CocoaPods versions"
-    sudo gem uninstall cocoapods --all --executables
-    echo "Installing CocoaPods version $CUR_COCOAPODS_VER"
-    sudo gem install cocoapods -v $CUR_COCOAPODS_VER
-else 
-    echo "CocoaPods version is suitable for the project"
-fi;
-echo "pod install"
+echo "Uninstalling all CocoaPods versions"
+sudo gem uninstall cocoapods --all --executables
+
+COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
+
+echo "Installing CocoaPods version $COCOAPODS_VER"
+sudo gem install cocoapods -v $COCOAPODS_VER
+
+cd $APPCENTER_SOURCE_DIRECTORY
 pod install
